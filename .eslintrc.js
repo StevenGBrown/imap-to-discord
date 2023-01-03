b1315fb730289d7f21fa7d30fb0f8bc61583963f
Steven Brown <StevenGBrown@gmail.com>
2023-01-03 11:34:09 +0800

    Drop eslint-plugin-import and eslint-plugin-node

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,21 +14,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'plugin:node/recommended-module',
     'plugin:prettier/recommended',
     'plugin:security/recommended',
   ],
-  rules: {
-    'node/no-missing-import': 'off', // covered by import plugin
-  },
-  settings: {
-    // eslint-import-resolver-typescript
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-      },
-    },
-  },
 }

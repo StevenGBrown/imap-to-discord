51f08de8eb0dbf636a058db938b831355ec65c3f
Steven Brown <StevenGBrown@gmail.com>
2023-11-23 11:32:17 +0800

    Replace node-imap with imapflow
    - Last published a month ago
    - Resolves npm audit warnings

--- lib/imap.ts
+++ lib/imap.ts
@@ -1,50 +1,34 @@
-import Connection from 'node-imap'
+import { ImapFlow, MailboxObject } from 'imapflow'
 import * as stream from 'node:stream'
 
+import { Configuration } from './configuration'
+
 export class Imap {
-  static async connect(config: Connection.Config): Promise<Imap> {
-    const connection = new Connection({ authTimeout: 60000, ...config })
-    await new Promise<void>((resolve) => {
-      connection.once('ready', resolve)
-      connection.connect()
-    })
-    return new Imap(connection)
+  static async connect(
+    config: Pick<Configuration, 'host' | 'port' | 'tls' | 'user' | 'password'>
+  ): Promise<Imap> {
+    const { host, port, tls: secure, user, password: pass } = config
+    const client = new ImapFlow({ host, port, secure, auth: { user, pass } })
+    await client.connect()
+    return new Imap(client)
   }
 
-  private constructor(private readonly connection: Connection) {}
+  private constructor(private readonly client: ImapFlow) {}
 
   public async openMailbox({
     folder,
   }: {
     folder: string
   }): Promise<ImapMailbox> {
-    const openReadOnly = false // allow marking as read
-    return await new Promise((resolve, reject) =>
-      this.connection.openBox(folder, openReadOnly, (err, box) => {
-        if (!box.persistentUIDs) {
-          // This means that the UIDs change every time the mailbox is opened,
-          // so we can't tell which emails have already been sent to Discord.
-          err =
-            err ??
-            new Error(
-              'This mailbox is not supported because it does not use persistent UIDs.'
-            )
-        }
-
-        if (err) {
-          reject(err)
-        } else {
-          resolve(new ImapMailbox(this.connection, box))
-        }
-      })
-    )
+    const mailbox = await this.client.mailboxOpen(folder, {
+      // allow marking as read
+      readOnly: false,
+    })
+    return new ImapMailbox(this.client, mailbox)
   }
 
   public async disconnect(): Promise<void> {
-    await new Promise((resolve) => {
-      this.connection.once('close', resolve)
-      this.connection.end()
-    })
+    await this.client.logout()
   }
 }
 
@@ -52,55 +36,30 @@ export class ImapMailbox {
   public readonly uidnext: number
   public readonly uidvalidity: number
 
-  constructor(private readonly connection: Connection, box: Connection.Box) {
-    this.uidnext = box.uidnext
-    this.uidvalidity = box.uidvalidity
+  constructor(private readonly client: ImapFlow, mailbox: MailboxObject) {
+    this.uidnext = mailbox.uidNext
+    this.uidvalidity = Number(mailbox.uidValidity) // 32 bits, fits within MAX_SAFE_INTEGER
   }
 
   public async searchFromUid(uid: number): Promise<{ uids: number[] }> {
-    const searchCriteria = [['UID', `${uid}:*`]]
-    return await new Promise((resolve, reject) =>
-      this.connection.search(searchCriteria, (error, uids) =>
-        error
-          ? reject(error)
-          : resolve({
-              // Always includes the UID of the latest email even if it is outside of the requested range
-              // https://stackoverflow.com/questions/9147424/imap-search-for-messages-with-uid-greater-than-x-or-generally-after-my-last-s#comment29989969_9148609
-              uids: uids.filter((value) => value >= uid).sort((a, b) => a - b),
-            })
-      )
-    )
+    const uids = await this.client.search({ uid: `${uid}:*` }, { uid: true })
+    return {
+      uids: uids
+        // Includes the UID of the latest email even if it is outside of the requested range
+        // https://stackoverflow.com/questions/9147424/imap-search-for-messages-with-uid-greater-than-x-or-generally-after-my-last-s#comment29989969_9148609
+        .filter((value) => value >= uid)
+        .sort((a, b) => a - b),
+    }
   }
 
-  public async getEmailContent(uid: number): Promise<stream.Readable | null> {
-    const imapFetch = fixType(this.connection.fetch(uid, { bodies: '' }))
-    return await new Promise((resolve, reject) => {
-      imapFetch.once('message', (message) => {
-        fixType(message).once('body', (content) => {
-          resolve(new stream.Readable().wrap(content))
-        })
-      })
-      imapFetch.once('error', (error) => reject(error))
-      imapFetch.once('end', () => {
-        resolve(null)
-      })
+  public async getEmailContent(uid: number): Promise<stream.Readable> {
+    const message = await this.client.download(`${uid}`, undefined, {
+      uid: true,
     })
+    return message.content
   }
 
   public async markAsRead(uid: number): Promise<void> {
-    await new Promise<void>((resolve, reject) =>
-      this.connection.addFlags(uid, 'Seen', (error) =>
-        error ? reject(error) : resolve()
-      )
-    )
+    await this.client.messageFlagsAdd(`${uid}`, ['\\Seen'], { uid: true })
   }
-}
-
-/**
- * Use the `on` function types for the `once` function too.
- */
-function fixType<EventEmitter extends NodeJS.EventEmitter>(
-  eventEmitter: EventEmitter
-): EventEmitter & { once: EventEmitter['on'] } {
-  return eventEmitter
 }
